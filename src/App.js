52d638c6e0b9118ea13acd379867c53540898f43
Dinesh Katariya <thedineshk24@gmail.com>
2022-07-20 13:37:03 +0530

    added container

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import MultiStepper from './components/MultiStepper';
 import MyErrorBoundary from './ErrorBoundry/MyErrorBoundry';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 function App() {
   return (
     <div className="App">
-      <MyErrorBoundary>
-        <MultiStepper />
-      </MyErrorBoundary>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <MyErrorBoundary>
+          <MultiStepper />
+        </MyErrorBoundary>
+      </Container>
     </div>
   );
 }
